304231a0e7442fdd80d7580fba4d227fe24a11a2
Irvin Martinez <100785904+Irvin-Mx@users.noreply.github.com>
2025-04-28 22:00:59 +0000

    Fix navbar register button

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -24,12 +24,12 @@ export const Navbar = () => {
 				<div className="collapse navbar-collapse" id="navbarSupportedContent">
 					<div className="w-100">
 						<div className="d-grid gap-2 d-md-flex justify-content-md-end">
-							{/* if user in login, show register button */}
+							{/* if user in home, show register button */}
 							{location.pathname === "/"? <Link to="/registro"><button className={`${styles.navbar__btn__register} btn me-md-2`} type="button">Registrarse</button></Link>  : null}
 							{location.pathname === "/"? <Link to="/login"><button className={`${styles.navbar__btn__login} btn me-md-2`} type="button">Iniciar sesion</button></Link>  : null}
 							
 							{/* if user in register, show login button */}
-							{location.pathname === "/register"? <Link to="/login"><button className={`${styles.navbar__btn__login} btn me-md-2`} type="button">Iniciar sesion</button></Link> : null}
+							{location.pathname === "/registro"? <Link to="/login"><button className={`${styles.navbar__btn__login} btn me-md-2`} type="button">Iniciar sesion</button></Link> : null}
 							
 							{/* if user in login, show register button */}
 							{location.pathname === "/login"? <Link to="/registro"><button className={`${styles.navbar__btn__register} btn me-md-2`} type="button">Registrarse</button></Link> : null}
