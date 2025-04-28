d791f884aeb0b3a46e08815b27ad6680679e6dbe
JuanEspinoH <108641768+JuanEspinoH@users.noreply.github.com>
2025-04-28 20:11:40 +0000

    feat modelo de servicio y relacion con usuario

--- src/api/models.py
+++ src/api/models.py
@@ -1,5 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timezone
+from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+from decimal import Decimal
 
 db = SQLAlchemy()
 
@@ -16,6 +19,8 @@ class User(db.Model):
     is_active = db.Column(db.Boolean(), nullable=False,default=True)
     created_at = db.Column(db.DateTime(timezone=True), default=datetime.now(timezone.utc), nullable=False)
 
+    services = relationship("Service", back_populates="user")
+
 
     def serialize(self):
         return {
@@ -27,5 +32,30 @@ class User(db.Model):
             "address": self.address,
             "rol": self.rol,
             "is_active": self.is_active,
+            # do not serialize the password, its a security breach
+        }
+
+class Service(db.Model):
+    __tablename__="services"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=True)
+    price = db.Column(db.DECIMAL(precision=10, scale=2), nullable=True)
+    description = db.Column(db.String(200), nullable=True)
+    img_url=db.Column(db.String(), nullable=True)
+
+    
+    created_at = db.Column(db.DateTime(timezone=True), default=datetime.now(timezone.utc), nullable=False)
+    
+    usuario_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship("User", back_populates="services")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "price": self.price,
+            "description": self.description,
+            "img_url": self.img_url,
+            "usuario_id": self.usuario_id,
             # do not serialize the password, its a security breach
         }
