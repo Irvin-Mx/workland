4c6cfc0eea7d27549281211b60c54c373cc24086
Irvin Martinez <100785904+Irvin-Mx@users.noreply.github.com>
2025-04-28 21:40:38 +0000

    Feature vista home

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,22 +1,41 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, {useEffect} from "react";
+import { Link,useLocation } from "react-router-dom";
 
 //syles
 import styles from "./navbar.module.css"
 
 export const Navbar = () => {
+	let location = useLocation()
+
+	// useEffect(()=>{
+	// 	let location = useLocation()
+	// 	console.log(location)
+	// })
+	useEffect(()=>{
+		console.log(location.pathname)
+	})
+
+
 	return (
 		<nav className={`${styles.navbar__container} navbar navbar-expand-lg bg-body-tertiary`}>
 			<div className="container-fluid">
-				<a className={`${styles.navbar__brand} navbar-brand fs-2 `} href="#">Workland</a>
+				{/* <a className= href="">Workland</a> */}
+				<Link to="/" className={`${styles.navbar__brand} navbar-brand fs-2 `} >Workland</Link>
 				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 					<span className="navbar-toggler-icon"></span>
 				</button>
 				<div className="collapse navbar-collapse" id="navbarSupportedContent">
 					<div className="w-100">
-						<div class="d-grid gap-2 d-md-flex justify-content-md-end">
-							<button className={`${styles.navbar__btn__register} btn me-md-2`} type="button">Registrarse</button>
-							<button className={`${styles.navbar__btn__login} btn me-md-2`} type="button">Iniciar sesion</button>
+						<div className="d-grid gap-2 d-md-flex justify-content-md-end">
+							{/* if user in login, show register button */}
+							{location.pathname === "/"? <Link to="/registro"><button className={`${styles.navbar__btn__register} btn me-md-2`} type="button">Registrarse</button></Link>  : null}
+							{location.pathname === "/"? <Link to="/login"><button className={`${styles.navbar__btn__login} btn me-md-2`} type="button">Iniciar sesion</button></Link>  : null}
+							
+							{/* if user in register, show login button */}
+							{location.pathname === "/register"? <Link to="/login"><button className={`${styles.navbar__btn__login} btn me-md-2`} type="button">Iniciar sesion</button></Link> : null}
+							
+							{/* if user in login, show register button */}
+							{location.pathname === "/login"? <Link to="/registro"><button className={`${styles.navbar__btn__register} btn me-md-2`} type="button">Registrarse</button></Link> : null}
 						</div>
 					</div>
 				</div>
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -11,42 +11,42 @@ export const Home = () => {
 				<div className="d-flex flex-column align-items-center">
 					<h1>Workland</h1>
 					<h4>¡El trabajo de tus suenos, te espera!</h4>
-					<div class="input-group mb-3 w-50">
-						<input type="text" class="form-control" placeholder="Search" aria-label="Recipient's username" aria-describedby="button-addon2" />
-						<button class="btn btn-outline-secondary" type="button" id="button-addon2"><i class="fa-solid fa-magnifying-glass"></i></button>
+					<div className="input-group mb-3 w-50">
+						<input type="text" className="form-control" placeholder="Search" aria-label="Recipient's username" aria-describedby="button-addon2" />
+						<button className="btn btn-outline-secondary" type="button" id="button-addon2"><i className="fa-solid fa-magnifying-glass"></i></button>
 					</div>
 				</div>
 
 				<div className="d-flex flex-column justify-content-center align-items-center">
 					<div className="w-75">
 						{/* Card */}
-						<div class="card mb-3 ">
-							<div class="row g-0 ">
-								<div class="col-md-5 d-flex flex-column justify-content-center align-items-center p-4">
-									<img src="https://placehold.co/500x200" class="img-fluid rounded-start object-fit-contain" alt="..." />
+						<div className="card mb-3 ">
+							<div className="row g-0 ">
+								<div className="col-md-5 d-flex flex-column justify-content-center align-items-center p-4">
+									<img src="https://placehold.co/500x200" className="img-fluid rounded-start object-fit-contain" alt="..." />
 								</div>
-								<div class="col-md-7 d-flex align-items-center ">
-									<div class="card-body d-flex flex-column p-4">
-										<h5 class="card-title justify-content-start w-25  fs-3">Card title</h5>
-										<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris non mauris vehicula, laoreet dolor sed, fermentum ante. Proin eleifend molestie neque, pulvinar euismod sem. Pellentesque vel malesuada orci. Donec ornare convallis tincidunt. Curabitur vitae lobortis purus, et scelerisque ligula. Suspendisse et pretium ante. Pellentesque eget libero eget ex efficitur ultricies. Sed feugiat consequat tincidunt. Mauris elementum laoreet molestie. </p>
-										{/* <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p> */}
+								<div className="col-md-7 d-flex align-items-center ">
+									<div className="card-body d-flex flex-column p-4">
+										<h5 className="card-title justify-content-start w-25  fs-3">Card title</h5>
+										<p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris non mauris vehicula, laoreet dolor sed, fermentum ante. Proin eleifend molestie neque, pulvinar euismod sem. Pellentesque vel malesuada orci. Donec ornare convallis tincidunt. Curabitur vitae lobortis purus, et scelerisque ligula. Suspendisse et pretium ante. Pellentesque eget libero eget ex efficitur ultricies. Sed feugiat consequat tincidunt. Mauris elementum laoreet molestie. </p>
+										{/* <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p> */}
 									</div>
 								</div>
 							</div>
 						</div>
 
 						{/* Card */}
-						<div class="card mb-3">
-							<div class="row g-0 ">
-							<div class="col-md-7 d-flex align-items-center ">
-									<div class="card-body d-flex flex-column p-4">
-										<h5 class="card-title justify-content-start w-25  fs-3">Card title</h5>
-										<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris non mauris vehicula, laoreet dolor sed, fermentum ante. Proin eleifend molestie neque, pulvinar euismod sem. Pellentesque vel malesuada orci. Donec ornare convallis tincidunt. Curabitur vitae lobortis purus, et scelerisque ligula. Suspendisse et pretium ante. Pellentesque eget libero eget ex efficitur ultricies. Sed feugiat consequat tincidunt. Mauris elementum laoreet molestie. </p>
-										{/* <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p> */}
+						<div className="card mb-3">
+							<div className="row g-0 ">
+							<div className="col-md-7 d-flex align-items-center ">
+									<div className="card-body d-flex flex-column p-4">
+										<h5 className="card-title justify-content-start w-25  fs-3">Card title</h5>
+										<p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris non mauris vehicula, laoreet dolor sed, fermentum ante. Proin eleifend molestie neque, pulvinar euismod sem. Pellentesque vel malesuada orci. Donec ornare convallis tincidunt. Curabitur vitae lobortis purus, et scelerisque ligula. Suspendisse et pretium ante. Pellentesque eget libero eget ex efficitur ultricies. Sed feugiat consequat tincidunt. Mauris elementum laoreet molestie. </p>
+										{/* <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p> */}
 									</div>
 								</div>
-								<div class="col-md-5 d-flex flex-column justify-content-center align-items-center p-4">
-									<img src="https://placehold.co/500x200" class="img-fluid rounded-start object-fit-contain" alt="..." />
+								<div className="col-md-5 d-flex flex-column justify-content-center align-items-center p-4">
+									<img src="https://placehold.co/500x200" className="img-fluid rounded-start object-fit-contain" alt="..." />
 								</div>
 							</div>
 						</div>
