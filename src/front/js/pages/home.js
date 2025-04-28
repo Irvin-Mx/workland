035b1fce86ad8b79b8433eda92120c8265b2ccb0
Irvin Martinez <100785904+Irvin-Mx@users.noreply.github.com>
2025-04-28 16:39:10 +0000

    Create card

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -16,6 +16,38 @@ export const Home = () => {
 						<button class="btn btn-outline-secondary" type="button" id="button-addon2">Search</button>
 					</div>
 				</div>
+				<div>
+					{/* Card */}
+					<div class="card mb-3 h-[500px]">
+						<div class="row g-0 bg-success">
+							<div class="col-md-4 bg-danger">
+								<img src="https://placehold.co/300x100" class="img-fluid rounded-start" alt="..." />
+							</div>
+							<div class="col-md-8 d-flex align-items-center ">
+								<div class="card-body d-flex flex-column h-75 bg-primary">
+									<h5 class="card-title justify-content-start w-25 bg-warning">Card title</h5>
+									<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+									<p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="card mb-3" >
+						<div class="row g-0">
+							<div class="col-md-8">
+								<div class="card-body">
+									<h5 class="card-title">Card title</h5>
+									<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+									<p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+								</div>
+							</div>
+							<div class="col-md-4">
+								<img src="https://placehold.co/600x400" class="img-fluid rounded-start" alt="..." />
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
