57868c31d3d01e5f1df2056a86f4d74e7020cf54
Irvin Martinez <100785904+Irvin-Mx@users.noreply.github.com>
2025-04-28 20:56:40 +0000

    Modify navbar and home styles

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,41 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+//syles
+import styles from "./navbar.module.css"
+
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-expand-lg bg-body-tertiary">
+		<nav className={`${styles.navbar__container} navbar navbar-expand-lg bg-body-tertiary`}>
 			<div className="container-fluid">
-				<a className="navbar-brand" href="#">Workland</a>
+				<a className={`${styles.navbar__brand} navbar-brand fs-2 `} href="#">Workland</a>
 				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 					<span className="navbar-toggler-icon"></span>
 				</button>
 				<div className="collapse navbar-collapse" id="navbarSupportedContent">
-					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
-						<li className="nav-item">
-							<a className="nav-link active" aria-current="page" href="#">Home</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link" href="#">Link</a>
-						</li>
-						<li className="nav-item dropdown">
-							<a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-								Dropdown
-							</a>
-							<ul className="dropdown-menu">
-								<li><a className="dropdown-item" href="#">Action</a></li>
-								<li><a className="dropdown-item" href="#">Another action</a></li>
-								<li><hr className="dropdown-divider"/></li>
-								<li><a className="dropdown-item" href="#">Something else here</a></li>
-							</ul>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link disabled" aria-disabled="true">Disabled</a>
-						</li>
-					</ul>
-					<form className="d-flex" role="search">
-						<input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-							<button className="btn btn-outline-success" type="submit">Search</button>
-					</form>
+					<div className="w-100">
+						<div class="d-grid gap-2 d-md-flex justify-content-md-end">
+							<button className={`${styles.navbar__btn__register} btn me-md-2`} type="button">Registrarse</button>
+							<button className={`${styles.navbar__btn__login} btn me-md-2`} type="button">Iniciar sesion</button>
+						</div>
+					</div>
 				</div>
 			</div>
 		</nav>
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -8,42 +8,46 @@ export const Home = () => {
 	return (
 		<div className="text-center mt-5">
 			<div>
-				<div>
+				<div className="d-flex flex-column align-items-center">
 					<h1>Workland</h1>
 					<h4>¡El trabajo de tus suenos, te espera!</h4>
-					<div class="input-group mb-3">
+					<div class="input-group mb-3 w-50">
 						<input type="text" class="form-control" placeholder="Search" aria-label="Recipient's username" aria-describedby="button-addon2" />
-						<button class="btn btn-outline-secondary" type="button" id="button-addon2">Search</button>
+						<button class="btn btn-outline-secondary" type="button" id="button-addon2"><i class="fa-solid fa-magnifying-glass"></i></button>
 					</div>
 				</div>
-				<div>
-					{/* Card */}
-					<div class="card mb-3 h-[500px]">
-						<div class="row g-0 bg-success">
-							<div class="col-md-4 bg-danger">
-								<img src="https://placehold.co/300x100" class="img-fluid rounded-start" alt="..." />
-							</div>
-							<div class="col-md-8 d-flex align-items-center ">
-								<div class="card-body d-flex flex-column h-75 bg-primary">
-									<h5 class="card-title justify-content-start w-25 bg-warning">Card title</h5>
-									<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-									<p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+
+				<div className="d-flex flex-column justify-content-center align-items-center">
+					<div className="w-75">
+						{/* Card */}
+						<div class="card mb-3 ">
+							<div class="row g-0 ">
+								<div class="col-md-5 d-flex flex-column justify-content-center align-items-center p-4">
+									<img src="https://placehold.co/500x200" class="img-fluid rounded-start object-fit-contain" alt="..." />
+								</div>
+								<div class="col-md-7 d-flex align-items-center ">
+									<div class="card-body d-flex flex-column p-4">
+										<h5 class="card-title justify-content-start w-25  fs-3">Card title</h5>
+										<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris non mauris vehicula, laoreet dolor sed, fermentum ante. Proin eleifend molestie neque, pulvinar euismod sem. Pellentesque vel malesuada orci. Donec ornare convallis tincidunt. Curabitur vitae lobortis purus, et scelerisque ligula. Suspendisse et pretium ante. Pellentesque eget libero eget ex efficitur ultricies. Sed feugiat consequat tincidunt. Mauris elementum laoreet molestie. </p>
+										{/* <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p> */}
+									</div>
 								</div>
 							</div>
 						</div>
-					</div>
 
-					<div class="card mb-3" >
-						<div class="row g-0">
-							<div class="col-md-8">
-								<div class="card-body">
-									<h5 class="card-title">Card title</h5>
-									<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-									<p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+						{/* Card */}
+						<div class="card mb-3">
+							<div class="row g-0 ">
+							<div class="col-md-7 d-flex align-items-center ">
+									<div class="card-body d-flex flex-column p-4">
+										<h5 class="card-title justify-content-start w-25  fs-3">Card title</h5>
+										<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris non mauris vehicula, laoreet dolor sed, fermentum ante. Proin eleifend molestie neque, pulvinar euismod sem. Pellentesque vel malesuada orci. Donec ornare convallis tincidunt. Curabitur vitae lobortis purus, et scelerisque ligula. Suspendisse et pretium ante. Pellentesque eget libero eget ex efficitur ultricies. Sed feugiat consequat tincidunt. Mauris elementum laoreet molestie. </p>
+										{/* <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p> */}
+									</div>
 								</div>
-							</div>
-							<div class="col-md-4">
-								<img src="https://placehold.co/600x400" class="img-fluid rounded-start" alt="..." />
+								<div class="col-md-5 d-flex flex-column justify-content-center align-items-center p-4">
+									<img src="https://placehold.co/500x200" class="img-fluid rounded-start object-fit-contain" alt="..." />
+								</div>
 							</div>
 						</div>
 					</div>
