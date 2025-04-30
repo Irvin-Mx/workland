5db62ca4b6c4193158e2424aa618904639f5e56f
Sebastián <BICHOS1506@GMAIL.COM>
2025-04-30 23:21:38 +0000

    funcionalidad en el perfil del freelance

--- src/front/js/pages/freelanceConfiguracion.js
+++ src/front/js/pages/freelanceConfiguracion.js
@@ -1,11 +1,18 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
-import { Link } from "react-router-dom";
-import "../../styles/freelance.css";
+
+
 
 
 export const FreelanceConfiguracion = () => {
     const { store, actions } = useContext(Context);
+
+    useEffect(() => {
+        document.body.style.backgroundColor = "aliceblue";
+        return () => {
+            document.body.style.backgroundColor = '';
+        };
+    }, []);
 
     return (
         <div className="container">
@@ -22,14 +29,15 @@ export const FreelanceConfiguracion = () => {
                             padding: "8px 0",
                             fontSize: "30px",
                             color: "#1E266D",
+                            background: "aliceblue",
                         }} />
                     </div>
-                    <div className="card-title border rounded shadow w-50 m-3 ms-1">
-                        <div className="card-header" style={{ background: "#1E266D", color: "#ffffff", fontSize:"1.5rem" }}>
+                    <div className="card-title border rounded shadow w-50 m-3 ms-1" style={{ background: "aliceblue" }}>
+                        <div className="card-header" style={{ background: "#1E266D", color: "#ffffff", fontSize: "1.5rem" }}>
                             ¿Qué Producto vas a vender?
                         </div>
                         <div className="card-body">
-                            <div className="border p-4 rounded" style={{ background:"white",  justifyContent: "center" }}>
+                            <div className="border p-4 rounded" style={{ background: "white", justifyContent: "center" }}>
                                 <i className="fa-solid fa-image fa-8x" style={{ opacity: 0.5 }}></i>
                                 <div className="custom-file mb-4">
                                     <input type="file" className="custom-file-input" id="inputGroupFile04" />
@@ -43,8 +51,8 @@ export const FreelanceConfiguracion = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="card-description border rounded shadow w-50 m-3 ms-1">
-                        <div className="card-header" style={{ background: "#1E266D", color: "#ffffff",fontSize:"1.5rem" }} >
+                    <div className="card-description border rounded shadow w-50 m-3 ms-1" style={{ background: "aliceblue" }}>
+                        <div className="card-header" style={{ background: "#1E266D", color: "#ffffff", fontSize: "1.5rem" }} >
                             Información del Producto
                         </div>
                         <div className="card-body">
@@ -52,36 +60,40 @@ export const FreelanceConfiguracion = () => {
                             <div className="row">
                                 <div className="col">
                                     <label htmlFor="inputName" className="form-label">Nombre</label>
-                                    <input id="inputName" class="form-control" type="text" />
+                                    <input id="inputName" className="form-control" type="text" />
                                 </div>
                                 <div className="col">
                                     <label htmlFor="inputName" className="form-label">Categoría</label>
-                                    <input id="inputName" class="form-control" type="text" />
+                                    <input id="inputName" className="form-control" type="text" />
                                 </div>
                             </div>
                             <div className="text-area">
-                                <label for="exampleFormControlTextarea1">Descripción del producto</label>
-                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                <label htmlFor="exampleFormControlTextarea1">Descripción del producto</label>
+                                <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                             </div>
                         </div>
                     </div>
-                    <div className="card-package border rounded shadow w-50 m-3 ms-1" style={{background: "aliceblue"}}>
-                        <div className="card-header" style={{ background: "#1E266D", color: "#ffffff", fontSize:"1.5rem" }}>
-                        Precios
+                    <div className="card-package border rounded shadow w-50 m-3 ms-1" style={{ background: "aliceblue" }}>
+                        <div className="card-header" style={{ background: "#1E266D", color: "#ffffff", fontSize: "1.5rem" }}>
+                            Precios
                         </div>
                         <div className="card-body">
                             <h5>Tipo de paquete</h5>
                             <div className="row">
                                 <div className="col mb-3">
-                                    <label for="disabledSelect" class="form-label">Disabled select menu</label>
-                                    <select id="disabledSelect" class="form-select">
-                                        <option>Disabled select</option>
+                                    <label htmlFor="disabledSelect" className="form-label">Disabled select menu</label>
+                                    <select id="disabledSelect" className="form-select">
+                                        <option value="">Selecciona un paquete</option>
+                                        <option value="emprendedor">Basico</option>
+                                        <option value="emprendedor">Pro</option>
+                                        <option value="usuario">Empresarial</option>
+                                        
                                     </select>
                                 </div>
-                                <div class="input-group col mb-3">
-                                    <span class="input-group-text">$</span>
-                                    <span class="input-group-text">0.00</span>
-                                    <input type="text" class="form-control" aria-label="Dollar amount (with dot and two decimal places)" />
+                                <div className="input-group col mb-3">
+                                    <span className="input-group-text">$</span>
+                                    <span className="input-group-text">0.00</span>
+                                    <input type="text" className="form-control" aria-label="Dollar amount (with dot and two decimal places)" />
                                 </div>
                             </div>
                         </div>
--- src/front/js/pages/freelanceEdit.js
+++ src/front/js/pages/freelanceEdit.js
@@ -1,41 +1,67 @@
-import React, { useContext,useEffect, } from "react";
+import React, { useContext, useEffect, } from "react";
 import { Context } from "../store/appContext";
+import { Link } from "react-router-dom";
 
 export const FreelanceEdit = () => {
-const {store,actions}= useContext(Context);
+    const { store, actions } = useContext(Context);
 
-useEffect(() => {
-    actions.getMyProfile();
-}, []);
+    useEffect(() => {
+        actions.getMyProfile();
+    }, []);
 
-const user = store.userProfile || {};
-if (!user.name) return <p>Cargando datos del usuario...</p>;
+    const user = store.userProfile || {};
+    if (!user.name) return <p>Cargando datos del usuario...</p>;
 
-return (
-    <div  className="user-card d-flex my-3 p-3 border rounded">
-        <div className="col-md-3 d-flex justify-content-center">
-            <img src="" alt="user" className="rounded-circle" />
-        </div>
+    return (
+        <div className=" d-flex flex-column align-items-center vh-100" >
+            <h1 className="text-center m-5">¡Bienvenido a Workland!</h1>
+            <div className="user-card d-flex my-3 p-3 w-50 border-rounded">
+                <p>Bienvenido, proveedor
+                    En nuestra plataforma, tú eliges <strong>cómo</strong>, <strong>cuándo</strong> y <strong>cuánto</strong> trabajar. Aquí puedes ofrecer tus servicios y conectar con personas que valoran lo que haces. Nuestro compromiso es ayudarte a construir el trabajo de tus sueños, donde tú tienes el control, y tu talento encuentra las oportunidades que merece.</p>
 
-        <div className="col-md-6 d-flex flex-column align-items-start">
-            <h5 className="name">{user.name}</h5>
-            <h5 className="last_name">{user.last_name}</h5>
-            <p className="adress"><i className="fa-solid fa-location-dot me-3"></i>{user.address}</p>
-            <p className="phone"><i className="fa-solid fa-phone me-3"></i>{user.phone}</p>
-            <p className="email"><i className="fa-solid fa-envelope me-3"></i>{user.email}</p>
-        </div>
+            </div>
 
-        <div className="col-md-3 d-flex justify-content-end align-items-start">
-            <button
-                type="button"
-                className="btn btn-link">
-                <i className="fa-solid fa-trash"></i>
+            <div className="user-card d-flex my-3 p-3 w-50 border rounded rounded shadow">
+                <div className="col-md-3 d-flex justify-content-center">
+                    <img src="" alt="user" className="rounded-circle" />
+                </div>
+
+                <div className="col-md-6 d-flex flex-column align-items-start">
+                    <h5 className="name">{user.name}</h5>
+                    <h5 className="last_name">{user.last_name}</h5>
+                    <p className="adress"><i className="fa-solid fa-location-dot me-3"></i>{user.address}</p>
+                    <p className="phone"><i className="fa-solid fa-phone me-3"></i>{user.phone}</p>
+                    <p className="email"><i className="fa-solid fa-envelope me-3"></i>{user.email}</p>
+                </div>
+
+                <div className="col-md-3 d-flex justify-content-end align-items-start">
+                    <button
+                        type="button"
+                        className="btn btn-link">
+                        <i class="fa-solid fa-pen-to-square"></i>
+                    </button>
+                    <button
+                        type="button"
+                        className="btn btn-link">
+                        <i className="fa-solid fa-trash"></i>
+                    </button>
+
+
+                </div>
+            </div>
+            <div className="user-card d-flex flex-column p-1 w-50 align-items-center border-rounded">
+                <h3>¡Súmate hoy y haz que cada servicio cuente!</h3>
+            </div>
+            <p>Estas a un solo paso de onbtener el trabajo de tus sueños Sigue las instricciones para agregar tus servicios</p>
+            <Link to="/freeConfig">
+            <button type="button" className="btn ms-2" style={{ background: "#00D1B2", color: "aliceblue" }}>
+                Agregar Servicio
             </button>
-
+            </Link>
         
         </div>
-    </div>
-);
+    );
 };
 
 export default FreelanceEdit;
+
