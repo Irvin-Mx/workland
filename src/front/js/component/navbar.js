5b9f8ac1fc5310cc1c3af2cb9ef6afa22d98b854
JuanEspinoH <108641768+JuanEspinoH@users.noreply.github.com>
2025-04-30 23:52:29 +0000

    fix navbar busqueda

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -5,6 +5,7 @@ import { Context } from "../store/appContext.js";
 //syles
 import styles from "./navbar.module.css"
 
+
 export const Navbar = () => {
 	const {store,actions} = useContext(Context)
 	let location = useLocation()
@@ -26,26 +27,42 @@ export const Navbar = () => {
 	return (
 		<nav className={`${styles.navbar__container} navbar navbar-expand-lg bg-body-tertiary`}>
 			<div className="container-fluid">
-				{/* <a className= href="">Workland</a> */}
+				<div className="d-flex justify-content-between align-items-center flex-row">
 				<Link to="/" className={`${styles.navbar__brand} navbar-brand fs-2 `} >Workland</Link>
+				{
+					store.userProfile?.name && <p className="m-0 text-white">{`de ${store.userProfile.name}`}</p>
+				}
+				
+				
+				</div>
+				
+				
 				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-					<span className="navbar-toggler-icon"></span>
+					<span className="navbar-toggler-icon">x</span>
 				</button>
 				<div className="collapse navbar-collapse" id="navbarSupportedContent">
 					<div className="w-100">
 						<div className="d-grid gap-2 d-md-flex justify-content-md-end">
 							{/* if user in home, show register button */}
-							{location.pathname === "/" && !localStorage.userToken? <Link to="/registro"><button className={`${styles.navbar__btn__register} btn me-md-2`} type="button">Registrarse</button></Link>  : null}
-							{location.pathname === "/" && !localStorage.userToken? <Link to="/iniciar-sesion"><button className={`${styles.navbar__btn__login} btn me-md-2`} type="button">Iniciar sesion</button></Link>  : null}
-							<Link to="/iniciar-sesion"><button className={`${styles.navbar__btn__login} btn me-md-2`} type="button">Iniciar sesion</button></Link>
+							{/* {location.pathname === "/" && !localStorage.userToken? <Link to="/registro"><button className={`${styles.navbar__btn__register} btn me-md-2`} type="button">Registrarse</button></Link>  : null}
+							{location.pathname === "/" && !localStorage.userToken? <Link to="/iniciar-sesion"><button className={`${styles.navbar__btn__login} btn me-md-2`} type="button">Iniciar sesion</button></Link>  : null} */}
+
+							{/* <Link to="/iniciar-sesion"><button className={`${styles.navbar__btn__login} btn me-md-2`} type="button">Iniciar sesion</button></Link> */}
+	
 							{/* if user in register, show login button */}
-							{location.pathname === "/registro"? <Link to="/iniciar-sesion"><button className={`${styles.navbar__btn__login} btn me-md-2`} type="button">Iniciar sesion</button></Link> : null}
+							{/* {location.pathname === "/registro"? <Link to="/iniciar-sesion"><button className={`${styles.navbar__btn__login} btn me-md-2`} type="button">Iniciar sesion</button></Link> : null} */}
 							
 							{/* if user in login, show register button */}
-							{location.pathname === "/iniciar-sesion"? <Link to="/registro"><button className={`${styles.navbar__btn__register} btn me-md-2`} type="button">Registrarse</button></Link> : null}
+							{/* {location.pathname === "/iniciar-sesion"? <Link to="/registro"><button className={`${styles.navbar__btn__register} btn me-md-2`} type="button">Registrarse</button></Link> : null} */}
 
 							{/* if token exists in localstorage or in store, show logout button */}
-							{localStorage.userToken || store.userToken? <Link to="/"><button onClick={handleLogOut} className={`${styles.navbar__btn__register} btn me-md-2`} type="button">Logout</button></Link> : null}
+							{store.userProfile?.name ? <Link to="/"><button onClick={handleLogOut} className={`${styles.navbar__btn__register} btn me-md-2`} type="button">Logout</button></Link> :
+							<>
+							<Link to="/registro"><button className={`${styles.navbar__btn__register} btn me-md-2`} type="button">Registrarse</button></Link>
+
+							<Link to="/iniciar-sesion"><button className={`${styles.navbar__btn__login} btn me-md-2`} type="button">Iniciar sesion</button></Link>
+							</>
+							}
 						</div>
 					</div>
 				</div>
