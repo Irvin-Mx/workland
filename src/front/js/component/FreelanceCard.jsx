701bb047468df85e6158236e43e1c3c68858ae09
Irvin Martinez <100785904+Irvin-Mx@users.noreply.github.com>
2025-05-15 17:31:45 +0000

    hover freelance card commented

--- src/front/js/component/FreelanceCard.jsx
+++ src/front/js/component/FreelanceCard.jsx
@@ -3,21 +3,21 @@ import { useNavigate } from 'react-router-dom'
 
 const FreelanceCard = ({user_name,title,id,cover_img_url}) => {
     const navigate=useNavigate()
-    const [bgColor, setBgColor] = useState('#fff')
+    // const [bgColor, setBgColor] = useState('#fff')
 
     return (
-        <div
-            className="card m-3"
-            style={{
-                width: "18rem",
-                height: "20rem",
-                backgroundColor: bgColor,
-                transition: "background-color 0.3s"
-            }}
-            onMouseEnter={() => setBgColor('#00D1B2')} // hover color
-            onMouseLeave={() => setBgColor('#fff')}    // default color
-        >
-            
+        // <div
+        //     className="card m-3"
+        //     style={{
+        //         width: "18rem",
+        //         height: "20rem",
+        //         backgroundColor: bgColor,
+        //         transition: "background-color 0.3s"
+        //     }}
+        //     onMouseEnter={() => setBgColor('#00D1B2')} // hover color
+        //     onMouseLeave={() => setBgColor('#fff')}    // default color
+        // >
+        <div className="card m-3" style={{width: "18rem", height:"20rem"}}>
             <div className="card-img-top" style={{height:"9rem"}}>
                 <img src={cover_img_url} className='h-100 w-100 object-fit-scale'  alt="alt image"/>
             </div>
